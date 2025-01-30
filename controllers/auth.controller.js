3395bbb2e2af12511f9eccc50ab317d38ab3a643
miclemabasie <miclemabasie3@gmail.com>
2025-01-30 01:01:54 +0100

    authentication API

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,9 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-const signup = async (req, res, next) => {
+export const signup = async (req, res, next) => {
   // Hash the password before saving
   const { username, email, password } = req.body;
   if (password.length < 6) {
@@ -20,4 +21,37 @@ const signup = async (req, res, next) => {
   }
 };
 
-export default signup;
+export const login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    // Find the user by email and include password
+    const user = await User.findOne({ email }).select("+password").exec();
+    if (!user) {
+      return next(errorHandler("Invalid email or password", 401));
+    }
+
+    // Compare the entered password with the hashed password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      console.log(user.password);
+      return next(errorHandler("Invalid email or password", 401));
+    }
+
+    // Generate a JWT token
+    const token = jwt.sign(
+      { id: user._id, email: user.email }, // Payload
+      "daslkjfalskdjfASDJAsd", // Secret key (store this in your .env file)
+      { expiresIn: "7d" } // Token expiration time
+    );
+
+    // Send token and user info
+    res.status(200).json({
+      message: "User logged in successfully.",
+      token,
+      user: { id: user._id, username: user.username, email: user.email },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,16 @@
-const createUser = (req, res) => {
+import User from "../models/User.js";
+
+// list all users in the system
+export const listUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createUser = (req, res) => {
   console.log(req.body);
   res.send("creating User");
 };
-
-export default createUser;
