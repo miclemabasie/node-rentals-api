cb2077d842dc4ea30612cd95ad4392fb3c506823
miclemabasie <miclemabasie3@gmail.com>
2025-02-01 11:08:17 +0100

    controllers for cars and lands

--- controllers/car.controller.js
+++ controllers/car.controller.js
@@ -1,11 +1,112 @@
 import User from "../models/User.js";
+import Car from "../models/Car.js";
 
-const createCar = async (req, res) => {
-  const { username, email, password } = req.body;
-  const newUser = new User({ username, email, password });
-
-  await newUser.save();
-  res.status(201).json("User created successfully..");
+export const listCar = async (req, res) => {
+  try {
+    const cars = await Car.find();
+    res.status(200).json(cars);
+  } catch (error) {}
 };
 
-export default createCar;
+export const createCar = async (req, res, next) => {
+  try {
+    const { name, description, price, location, owner } = req.body;
+
+    // validate the owner's ID
+    if (!mongoose.Types.ObjectId.isValid(owner)) {
+      return next(errorHandler("Invalid Owner ID", 400));
+    }
+
+    // check if Owner with given id exists
+    const existingUser = await User.findById(owner);
+    if (!existingUser) {
+      return next(errorHandler("Owner not found", 404));
+    }
+
+    console.log(name, description, price, location, owner);
+    const newCar = new Car({
+      name,
+      description,
+      price,
+      location,
+      owner,
+    });
+    await newCar.save();
+    res.status(201).json({
+      message: "Car created successfully",
+      car: newCar,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateCar = async (req, res, next) => {
+  try {
+    const { id } = req.params; // get the query params
+    const updates = req.body; // get the data to be updated
+    // validate the car ID
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return next(errorHandler("Inavlid car ID", 400));
+    }
+
+    // try to get and update the car
+    const updatedCar = await Car.findByIdAndUpdate(id, updates, {
+      new: true, // Return the updated document
+      runValidators: true, // Ensure all model validators are applied
+    });
+
+    // check if the car was found and updated
+    if (!updatedCar) {
+      return next(errorHandler("car not found", 404));
+    }
+
+    res.status(200).json({
+      message: "car was updated successfully",
+      car: updatedCar,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getCarDetail = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    // Verify the car Id
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return next(errorHandler("Invalid car ID", 400));
+    }
+
+    // check if the prperty exists
+    const car = await Car.findById(id);
+
+    if (!car) {
+      return next(errorHandler("car with id not found", 404));
+    }
+
+    return res.status(200).json(car);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteCar = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    // verify the car id
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return next(errorHandler("Invalid car Id", 400));
+    }
+
+    const deletedCar = await Car.findByIdAndDelete(id);
+
+    if (!deletedCar) {
+      return next(errorHandler("car not found", 404));
+    }
+
+    res.status(204).json("car deleted successfully.");
+  } catch (error) {
+    next(error);
+  }
+};
--- controllers/land.controller.js
+++ controllers/land.controller.js
@@ -1,6 +1,112 @@
-const createLand = (req, res) => {
-  console.log(req.body);
-  res.send("creating Land");
+import User from "../models/User.js";
+import Land from "../models/Land.js";
+
+export const Land = async (req, res) => {
+  try {
+    const lands = await Land.find();
+    res.status(200).json(lands);
+  } catch (error) {}
 };
 
-export default createLand;
+export const createLand = async (req, res, next) => {
+  try {
+    const { name, description, price, location, owner } = req.body;
+
+    // validate the owner's ID
+    if (!mongoose.Types.ObjectId.isValid(owner)) {
+      return next(errorHandler("Invalid Owner ID", 400));
+    }
+
+    // check if Owner with given id exists
+    const existingUser = await User.findById(owner);
+    if (!existingUser) {
+      return next(errorHandler("Owner not found", 404));
+    }
+
+    console.log(name, description, price, location, owner);
+    const newLand = new Land({
+      name,
+      description,
+      price,
+      location,
+      owner,
+    });
+    await newLand.save();
+    res.status(201).json({
+      message: "land created successfully",
+      land: newLand,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateLand = async (req, res, next) => {
+  try {
+    const { id } = req.params; // get the query params
+    const updates = req.body; // get the data to be updated
+    // validate the land ID
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return next(errorHandler("Inavlid land ID", 400));
+    }
+
+    // try to get and update the land
+    const updatedLand = await Land.findByIdAndUpdate(id, updates, {
+      new: true, // Return the updated document
+      runValidators: true, // Ensure all model validators are applied
+    });
+
+    // check if the land was found and updated
+    if (!updatedLand) {
+      return next(errorHandler("land not found", 404));
+    }
+
+    res.status(200).json({
+      message: "land was updated successfully",
+      land: updatedLand,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getLandDetail = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    // Verify the land Id
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return next(errorHandler("Invalid land ID", 400));
+    }
+
+    // check if the prperty exists
+    const land = await Land.findById(id);
+
+    if (!land) {
+      return next(errorHandler("land with id not found", 404));
+    }
+
+    return res.status(200).json(land);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteLand = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    // verify the land id
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return next(errorHandler("Invalid land Id", 400));
+    }
+
+    const deletedland = await Land.findByIdAndDelete(id);
+
+    if (!deletedland) {
+      return next(errorHandler("land not found", 404));
+    }
+
+    res.status(204).json("land deleted successfully.");
+  } catch (error) {
+    next(error);
+  }
+};
